dda24a3922c694f9952618e9cd2c16a754e5113b
cmcc15 <cormacredmen@gmail.com>
2022-11-10 15:13:48 +0000

    Added my project

--- getting-started-master/app/src/static/js/app.js
+++ getting-started-master/app/src/static/js/app.js
@@ -51,10 +51,6 @@ function TodoListCard() {
 
     return (
         <React.Fragment>
-            <AddItemForm onNewItem={onNewItem} />
-            {items.length === 0 && (
-                <p className="text-center">No items yet! Add one above!</p>
-            )}
             {items.map(item => (
                 <ItemDisplay
                     item={item}
